15410396f192cfb381e445e11ac9067db1680d04
Daniel Bulant <admin@danbulant.eu>
2019-12-18 18:00:46 +0100

    Don't reinvent the wheel - use this as document

---
+++ src/document.php
@@ -0,0 +1,32 @@
+<?
+
+namespace flutter;// /flutter/document.php
+
+class document extends widget
+{
+    /**
+     * Create new document root
+     * @param opt array containing head and body
+     */
+    function __construct($opt){
+        $obj = (object) $opt;
+        $this->obj = $obj;
+        $this->head = (empty($obj->head) ? null : $obj->head);
+        $this->body = (empty($obj->body) ? null : $obj->body);
+    }
+    /**
+     * Build the document
+     */
+    function build(){
+        ?>
+        <html>
+        <head>
+            <? $this->tryBuild("head"); ?>
+        </head>
+        <body>
+            <? $this->tryBuild("body"); ?>
+        </body>
+        </html>
+        <?
+    }
+}
